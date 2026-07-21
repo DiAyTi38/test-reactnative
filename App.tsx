4f14d0bb0ba0f4c07b58c805a56da4084f153f37
Trần Tiến Đạt <trandat@MacBook-Pro-cua-Tran.local>
2026-07-21 08:56:08 +0700

    video 29

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  //data-type
+  //string
+  const[name, setName] = useState<string>("DiAyTi");
+
+  //number
+  const[age, setAge] = useState<number>(21);
+
+  //null, undifined, boolean
+  const test = false;
+
+  //object, array
+  const[person, setPerson] = useState([{
+    name: "DiAyTi",
+    age: 21
+  }]);
+
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.text}>test</Text>
+        <Text style={styles.text}>{name}</Text>
+        <Text style={styles.text}>{JSON.stringify(person)}</Text>
       </View>
       <Text style={styles.text}>Hello World 
         <Text style={styles.DiAyti}> DiAyTi</Text>
@@ -25,9 +44,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
     fontSize: 60,
     color: 'red',
+    paddingTop: 20,
+    paddingHorizontal: 20
   },
 });
