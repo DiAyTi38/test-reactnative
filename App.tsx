072b9877d1b06819dcd96392fd020eda032c4058
Trần Tiến Đạt <trandat@MacBook-Pro-cua-Tran.local>
2026-07-21 14:51:23 +0700

    video 32

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button , StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
@@ -56,13 +56,20 @@ export default function App() {
       onPress={() => alert("tap me")}
       />
 
-      <View style={{marginTop: 20, borderColor: "red", borderWidth: 1}}>
+      <ScrollView style={{
+        marginTop: 20, 
+        borderColor: "red", borderWidth: 1
+      }}>
         {todoList.map(todo => {
           return (
-            <Text style={styles.todo}>{todo.title}</Text>
+            <Text key = {todo.id}
+              style={styles.todo}
+            >
+              {todo.title}
+            </Text>
           )
         })}
-      </View>
+      </ScrollView>
     </View>
   );
 }
