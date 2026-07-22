d2a8a04b2f7dfc181f29f2477e4f07d6c4385285
Trần Tiến Đạt <trandat@MacBook-Pro-cua-Tran.local>
2026-07-22 19:13:01 +0700

    video 59

---
+++ src/app/product/index.tsx
@@ -0,0 +1,13 @@
+import { Link, router } from "expo-router";
+import { Button, Text, View } from "react-native"
+
+const ProductPage = () => {
+    
+    return (
+        <View>
+            <Text>Product page</Text>
+        </View>
+    )
+}
+
+export default ProductPage;
