437ed3efed24c1dd783c1785c9e1904e74db9603
Trần Tiến Đạt <trandat@MacBook-Pro-cua-Tran.local>
2026-07-27 07:57:05 +0700

    video 148

--- src/components/account/user.info.tsx
+++ src/components/account/user.info.tsx
@@ -1,6 +1,14 @@
 import ShareInput from "@/components/input/share.input";
 import { useCurrentApp } from "@/context/app.context";
-import { Image, Platform, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 const styles = StyleSheet.create({
   container: {
@@ -19,27 +27,34 @@ const UserInfo = () => {
 
   const baseImage = `${backend}/images/avatar`;
   return (
-    <View style={styles.container}>
-      <View style={{ alignItems: "center", gap: 5 }}>
-        <Image
-          style={{ height: 150, width: 150 }}
-          source={{ uri: `${baseImage}/${appState?.user.avatar}` }}
-        />
-        <Text>{appState?.user.name}</Text>
-      </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      style={{ flex: 1 }}
+    >
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
+          <View style={{ alignItems: "center", gap: 5 }}>
+            <Image
+              style={{ height: 150, width: 150 }}
+              source={{ uri: `${baseImage}/${appState?.user.avatar}` }}
+            />
+            <Text>{appState?.user.name}</Text>
+          </View>
 
-      <View style={{ marginTop: 20, gap: 20 }}>
-        <ShareInput title="Họ tên" value={appState?.user.name} />
+          <View style={{ marginTop: 20, gap: 20 }}>
+            <ShareInput title="Họ tên" value={appState?.user.name} />
 
-        <ShareInput
-          title="Email"
-          keyboardType="email-address"
-          value={appState?.user.email}
-        />
+            <ShareInput
+              title="Email"
+              keyboardType="email-address"
+              value={appState?.user.email}
+            />
 
-        <ShareInput title="Số điện thoại" value={appState?.user.phone} />
-      </View>
-    </View>
+            <ShareInput title="Số điện thoại" value={appState?.user.phone} />
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
